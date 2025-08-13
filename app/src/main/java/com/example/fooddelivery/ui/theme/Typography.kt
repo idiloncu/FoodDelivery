ecc62773c40931b3f32f751bae1042db2a9e5efc
İdil Öncü <oncuidil@gmail.com>
2025-08-14 02:50:16 +0300

    project created

---
+++ app/src/main/java/com/example/fooddelivery/ui/theme/Typography.kt
@@ -0,0 +1,77 @@
+package com.example.fooddelivery.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.fooddelivery.R
+
+private val UnboundedFontFamily = FontFamily(
+    Font(R.font.font_unbounded_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.font_unbounded_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.font_unbounded_bold, FontWeight.Bold, FontStyle.Normal)
+)
+
+@Immutable
+data class AppTyphography(
+    val headline: TextStyle,
+    val titleLarge: TextStyle,
+    val titleMedium: TextStyle,
+    val titleSmall: TextStyle,
+    val body: TextStyle,
+    val bodySmall: TextStyle,
+    val label: TextStyle
+)
+
+val LocalAppTyphography = staticCompositionLocalOf {
+    AppTyphography(
+        headline = TextStyle.Default,
+        titleLarge = TextStyle.Default,
+        titleMedium = TextStyle.Default,
+        titleSmall = TextStyle.Default,
+        body = TextStyle.Default,
+        bodySmall = TextStyle.Default,
+        label = TextStyle.Default
+    )
+}
+val extendedTyphography = AppTyphography(
+    headline = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    body = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    label = TextStyle(
+        fontFamily = UnboundedFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 11.sp
+    ),
+)
---
+++ app/src/main/java/com/example/fooddelivery/ui/theme/screen/components/ProductPreviewSection.kt
@@ -0,0 +1,57 @@
+package com.example.fooddelivery.ui.theme.screen.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.ui.theme.AppTheme
+
+@Composable
+fun ProductPreviewSection(modifier: Modifier = Modifier) {
+
+
+}
+
+@Composable
+private fun ActionBar(
+    modifier: Modifier = Modifier,
+    headline: String
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = headline,
+            style = AppTheme.typography.headline,
+            color = AppTheme.colors.onSecondarySurface
+        )
+    }
+}
+
+@Composable
+private fun CloseButton(
+    modifier: Modifier
+) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        color = AppTheme.colors.actionSurface,
+        contentColor = AppTheme.colors.secondarySurface
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+
+        )
+
+    }
+}
